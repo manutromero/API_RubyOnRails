b1c248ccd84086be7f2c1b95f7d7e197b353526f
Manuel Romero <manu8083el@gmail.com>
2019-01-01 21:38:31 -0500

    se crearon las rutas, los controladores y las vistar basicas, aun tiene un bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :todo_lists do
+    resources :todo_items
+  end
   resources :books
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "todo_list#index"
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_13_190208) do
+ActiveRecord::Schema.define(version: 2019_01_01_173416) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
     t.string "synopsis"
     t.string "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "todo_items", force: :cascade do |t|
+    t.string "content"
+    t.integer "todo_list_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
+  end
+
+  create_table "todo_lists", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
